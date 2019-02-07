49325e5e7de1ede15004080e02f86686e9eb0791
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-07 09:24:27 -0700

    adds comments to article show page

--- spec/features/user_sees_article_show_page_spec.rb
+++ spec/features/user_sees_article_show_page_spec.rb
@@ -3,6 +3,8 @@ describe "user sees one article" do
   describe "they link from the articles index" do
     it "displays info for one article" do
       article_1 = Article.create!(title: "Fun with rails", body: "all the directories!")
+      comment_1 = article_1.comments.create!(author_name: "Eric", body: "keep up the good work")
+      comment_2 = article_1.comments.create!(author_name: "Pynn", body: "keep it up")
 
       visit articles_path
 
@@ -10,6 +12,10 @@ describe "user sees one article" do
 
       expect(page).to have_content(article_1.title)
       expect(page).to have_content(article_1.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
     end
   end
 end
