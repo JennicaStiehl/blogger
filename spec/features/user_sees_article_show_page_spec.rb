3dad8e2ee23bc61a680f8f6da3d45c289fab8ab2
JennicaStiehl <jennica.stiehl@gmail.com>
2019-02-07 09:49:37 -0700

    adds comments controller, create action, partial comment form and all tests passing

--- spec/features/user_sees_article_show_page_spec.rb
+++ spec/features/user_sees_article_show_page_spec.rb
@@ -1,21 +1,38 @@
 require 'rails_helper'
 describe "user sees one article" do
+  before :each do
+    @article_1 = Article.create!(title: "Fun with rails", body: "i love all the directories!")
+    @comment_1 = @article_1.comments.create!(author_name: "Eric", body: "keep up the good work")
+    @comment_2 = @article_1.comments.create!(author_name: "Pynn", body: "keep it up")
+  end
   describe "they link from the articles index" do
     it "displays info for one article" do
-      article_1 = Article.create!(title: "Fun with rails", body: "all the directories!")
-      comment_1 = article_1.comments.create!(author_name: "Eric", body: "keep up the good work")
-      comment_2 = article_1.comments.create!(author_name: "Pynn", body: "keep it up")
 
       visit articles_path
 
-      click_link article_1.title
+      click_link @article_1.title
 
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_1.body)
-      expect(page).to have_content(comment_1.author_name)
-      expect(page).to have_content(comment_1.body)
-      expect(page).to have_content(comment_2.author_name)
-      expect(page).to have_content(comment_2.body)
+      expect(page).to have_content(@article_1.title)
+      expect(page).to have_content(@article_1.body)
+      expect(page).to have_content(@comment_1.author_name)
+      expect(page).to have_content(@comment_1.body)
+      expect(page).to have_content(@comment_2.author_name)
+      expect(page).to have_content(@comment_2.body)
+    end
+  end
+  describe "they fill in a comment form" do
+    it "displays the comment on the article show page" do
+
+      visit article_path(@article_1)
+
+      fill_in "comment[author_name]", with: "me"
+      fill_in "comment[body]", with: "so many thoughts."
+      click_on "Submit"
+
+      expect(current_path).to eq(article_path(@article_1))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("me")
+      expect(page).to have_content("so many thoughts.")
     end
   end
 end
